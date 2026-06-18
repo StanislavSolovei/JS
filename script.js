367266229c5a8e4b9dab38303abbcf7d815a1ebc
StanislavSolovei <edukacjateb176@gmail.com>
2026-06-18 16:28:24 +0200

    Movies

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-let movies = [
-  { id: 1, title: "Интерстеллар", isWatched: true },
+const movies = [
+  { id: 1, title: "Интерстеллар", isWatched: false },
   { id: 2, title: "Матрица", isWatched: false },
   { id: 3, title: "Дюна", isWatched: false }
 ];
 
-const containerMovies = document.querySelector(".movie");
-const movieWriten = document.querySelector("movieInput");
-const send = document.querySelector("addBtn");
+const containerMovies = document.querySelector(".container");
+const movieWriten = document.querySelector("#movieInput");
+const send = document.querySelector("#addBtn");
 const movieLenght = movies.length;
 
 function renderMovies(){
   containerMovies.textContent = "";
-  for(let i = 0; i > movieLenght; i++){
-    
-  }
+  movies.forEach((movie) => {
+    const section = document.createElement("section");
+    const paragraf = document.createElement("p");
+    const button = document.createElement("button"); 
+    paragraf.textContent = movie.title;
+    button.textContent = "Просмотренно";
+    section.classList.add("movie");
+    button.addEventListener('click', () => {
+        paragraf.classList.add("isWatched")    
+        movie.isWatched = true;
+    })
+    if(movie.isWatched === true){
+        paragraf.classList.add("isWatched")
+    } 
+    section.appendChild(paragraf);
+    section.appendChild(button);
+    containerMovies.appendChild(section);
+  })  
 }
+
+send.addEventListener('click', () => {
+    const text = movieWriten.value;
+    const newMovie = {
+        id: movies.lenght + 1,
+        title: text,
+        isWatched: false
+    }
+    movies.push(newMovie);
+    renderMovies();
+})
+
+renderMovies();
